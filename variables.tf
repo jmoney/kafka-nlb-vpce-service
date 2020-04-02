f6cabbbec2f979affc4709b2c8ba9150e30ae897
Jonathan Monette <jon.monette@gmail.com>
2020-04-02 18:41:43 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_id" {}
+variable "subnets" {}
+variable "vpce_whitelisted_accounts" {}
+variable "kafka_instance_ids" {}
+variable "kafka_broker_count" {}
+variable "kafka_port" {
+    default = "6000"
+}
